a0ba09c6a1909b8c67c3ea1c57ed31b319176f1d
Momen <132298128+Mano4life@users.noreply.github.com>
2024-02-29 20:09:32 +0200

    section 2 & 3

--- profile.php
+++ profile.php
@@ -30,19 +30,22 @@
 
         <div class="sections">
 
-            <div class="partition <?php if (!isset($_GET['page']) || !in_array($_GET['page'], array(2, 3))) echo "active"; ?>">
+            <div class="partition <?php if (!isset($_GET['page']) || !in_array($_GET['page'], array(2, 3)))
+                echo "active"; ?>">
                 <a href="profile.php">تفاصيل الحساب</a>
             </div>
 
             <div class="line desktop-view"></div>
 
-            <div class="partition <?php if (isset($_GET['page']) && $_GET['page'] == 2) echo "active"; ?>">
+            <div class="partition <?php if (isset($_GET['page']) && $_GET['page'] == 2)
+                echo "active"; ?>">
                 <a href="profile.php?page=2">دوراتي</a>
             </div>
 
             <div class="line desktop-view"></div>
 
-            <div class="partition <?php if (isset($_GET['page']) && $_GET['page'] == 3) echo "active"; ?>">
+            <div class="partition <?php if (isset($_GET['page']) && $_GET['page'] == 3)
+                echo "active"; ?>">
                 <a href="profile.php?page=3">شهاداتي</a>
             </div>
 
@@ -59,12 +62,13 @@
     <!-- you have a general class [mobile-view -> to make this element display from 768px to below]  -->
     <!-- you have a general class [desktop-view -> to make this element display from 768px to above] -->
     <!-- !! Delete comments after start work !! -->
+    <!-- (dont understand their use + please put a comment breaker before each section to ease navigating the code) -->
     <section id="section-content">
         <!-- go down and start work at section-content-thePartOfPage[2,3] -->
         <!-- else in css the start in ready  -->
         <!-- !! Delete comments after start work !! -->
 
-        <?php if (!isset($_GET['page']) || !in_array($_GET['page'], array(2, 3))) : ?>
+        <?php if (!isset($_GET['page']) || !in_array($_GET['page'], array(2, 3))): ?>
 
             <section class="section-content-1">
                 <h1 class="section-name text-center desktop-view">حسابي</h1>
@@ -94,8 +98,11 @@
                                 <div class="down">
                                     <div class="image">
 
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53" fill="none">
-                                            <path d="M23.1875 39.75V12.7531L14.575 21.3656L9.9375 16.5625L26.5 0L43.0625 16.5625L38.425 21.3656L29.8125 12.7531V39.75H23.1875ZM6.625 53C4.80313 53 3.24294 52.3507 1.94444 51.0522C0.645943 49.7537 -0.00220271 48.1947 5.62394e-06 46.375V36.4375H6.625V46.375H46.375V36.4375H53V46.375C53 48.1969 52.3507 49.7571 51.0522 51.0556C49.7537 52.3541 48.1947 53.0022 46.375 53H6.625Z" fill="#BDBDBD" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53"
+                                            fill="none">
+                                            <path
+                                                d="M23.1875 39.75V12.7531L14.575 21.3656L9.9375 16.5625L26.5 0L43.0625 16.5625L38.425 21.3656L29.8125 12.7531V39.75H23.1875ZM6.625 53C4.80313 53 3.24294 52.3507 1.94444 51.0522C0.645943 49.7537 -0.00220271 48.1947 5.62394e-06 46.375V36.4375H6.625V46.375H46.375V36.4375H53V46.375C53 48.1969 52.3507 49.7571 51.0522 51.0556C49.7537 52.3541 48.1947 53.0022 46.375 53H6.625Z"
+                                                fill="#BDBDBD" />
                                         </svg>
                                     </div>
                                 </div>
@@ -142,15 +149,98 @@
                     </div>
                 </form>
             </section>
-        <?php elseif ($_GET['page'] == 2) : ?>
 
-            <section class="section-content-2 border">
-                عيش يا باشااااااااااا
+            <!-- =============================
+            ============ section 2 ============
+            ================================== -->
+        <?php elseif ($_GET['page'] == 2): ?>
+
+            <section class="section-content-2">
+                <h1 class="section-2-title">دورات</h1>
+                <div class="content">
+                    <div id="profile-courses" class="card-container d-flex justify-content-center gap-5 flex-wrap">
+                        <?php for ($i = 1; $i <= 9; $i++): ?>
+                            <!-- !! this should be turned into a dynamic card once we begin backend !! -->
+                            <div class="course-preview-card" style="width:285px;height:290px;">
+                                <img src="media/main/autocad.png" alt="card-pic" class="card-pic">
+                                <div class="category">الرسم الهندسي</div>
+                                <div class="title">دورة الأوتوكاد</div>
+                                <div class="author">دورة مقدما من قبل المهندس: محمود معتصم</div>
+                                <div class="description">
+                                    يُعد الأمن السيبراني أحد أهم التحديات التي تُواجه الشركات والمؤسسات
+                                    في عصرنا الحالي، حيث تت
+                                </div>
+                            </div>
+                        <?php endfor ?>
+                    </div>
+
+
+                    <?php include
+                        "main-component/scroll-buttons.php";
+                    getScrollButtons("profile-courses", 333); ?>
+
+
+                    <nav class="page-pagination">
+                        <ul class="pagination justify-content-center">
+                            <div class="cont d-flex justify-content-center align-items-center">
+
+                                <li class="page-item-nav prev d-flex flex-column justify-content-center">
+                                    <a class="page-link-nav d-flex justify-content-evenly gap-1" href="#">
+                                        <svg style="transform: rotate(180deg);" width="15" height="16" viewBox="0 0 7 12"
+                                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M0.75 6C0.75 5.75391 0.832031 5.53516 0.996093 5.37109L5.37109 0.996094C5.69922 0.640625 6.27344 0.640625 6.60156 0.996094C6.95703 1.32422 6.95703 1.89844 6.60156 2.22656L2.85547 6L6.60156 9.74609C6.95703 10.0742 6.95703 10.6484 6.60156 10.9766C6.27344 11.332 5.69922 11.332 5.37109 10.9766L0.996093 6.60156C0.832031 6.4375 0.75 6.21875 0.75 6Z"
+                                                fill="black"></path>
+                                        </svg>
+                                        <h6 style="margin: 0;">التالي</h6>
+                                    </a>
+                                </li>
+
+                                <li class="page-item">
+                                    <a class="page-link" href="#">50</a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">...</a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">3</a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">2</a>
+                                </li>
+                                <li class="page-item pagination-active">
+                                    <a class="page-link" href="#">1</a>
+                                </li>
+
+                                <li class="page-item-nav next d-flex flex-column justify-content-center">
+                                    <a class="page-link-nav d-flex justify-content-evenly gap-1" href="#">
+                                        <h6 style="margin: 0;">السابق</h6>
+                                        <svg width="15" height="16" viewBox="0 0 7 12" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M0.75 6C0.75 5.75391 0.832031 5.53516 0.996093 5.37109L5.37109 0.996094C5.69922 0.640625 6.27344 0.640625 6.60156 0.996094C6.95703 1.32422 6.95703 1.89844 6.60156 2.22656L2.85547 6L6.60156 9.74609C6.95703 10.0742 6.95703 10.6484 6.60156 10.9766C6.27344 11.332 5.69922 11.332 5.37109 10.9766L0.996093 6.60156C0.832031 6.4375 0.75 6.21875 0.75 6Z"
+                                                fill="black"></path>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </div>
+                        </ul>
+                    </nav>
+                </div>
+
             </section>
-        <?php else : ?>
+            <!-- =============================
+            ============ section 3 ============
+            ================================== -->
+        <?php else: ?>
 
-            <section class="section-content-3 border">
-                عيش يا باشااااااااااا
+            <section class="section-content-3">
+                <h1 class="section-3-title">شهاداتي</h1>
+                <!-- condition if related content exists echoes it out from database  -->
+                <!-- else print out the below message -->
+                <div class="content">
+                    <h5 class="profile-no-content">عذرا لاكنك لم تكمل اي دورة بعد حتى تحصل على الشهادة</h5>
+                </div>
             </section>
 
         <?php endif; ?>
